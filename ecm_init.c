f2b9453ee86392a71435e25b760a1091bf178a5a
Murat Sezgin <msezgin@codeaurora.org>
2014-12-05 14:53:53 -0800

    [qca-nss-ecm] Add DT condition.
    Do not initialize the ECM features, if the platform doesn't match
    with the ones in the device tree (DT).
    
    Change-Id: I79ae6dd6a94c3804fca2abf4dcb4ba5419e0a04c
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- ecm_init.c
+++ ecm_init.c
@@ -14,6 +14,7 @@
  **************************************************************************
  */
 #include <linux/module.h>
+#include <linux/of.h>
 
 extern int ecm_tracker_init(void);
 extern void ecm_tracker_exit(void);
@@ -79,6 +80,16 @@ extern void ecm_classifier_dscp_exit(void);
 static int __init ecm_init(void)
 {
 	int ret;
+
+	/*
+	 * Run only for IPQ8064 platform, if the device tree is used.
+	 */
+#ifdef CONFIG_OF
+	if (!of_machine_is_compatible("qcom,ipq8064")) {
+		printk(KERN_WARNING "Not compatible platform for ECM\n");
+		return 0;
+	}
+#endif
 	printk(KERN_INFO "ECM init\n");
 
 	ret = ecm_tracker_init();
@@ -211,6 +222,17 @@ err_db:
 static void __exit ecm_exit(void)
 {
 	printk(KERN_INFO "ECM exit\n");
+
+	/*
+	 * If the platform is not IPQ8064 and device tree is enabled,
+	 * this means ECM started but none of the features are used.
+	 * So, just return here.
+	 */
+#ifdef CONFIG_OF
+	if (!of_machine_is_compatible("qcom,ipq8064")) {
+		return;
+	}
+#endif
 
 	/* call stop on anything that requires a prepare-to-exit signal */
 	printk(KERN_INFO "stop conntrack notifier\n");
