60de7c3d9ef810b8d7e1f74fb383e5e6cc0c3caf
Kiran Kumar C.S.K <kkumarcs@codeaurora.org>
2015-07-24 23:34:58 +0530

    Multicast MFC update causing ECM assert
    Description: Remove assert and avoid processing MFC update when
    CI not found for multicast tuple. This race condition will be
    resolved when patch 39529 is merged next week, which couples
    multicast CI and multicast tuple objects, so that they are
    allocated and freed atomically.
    
    Change-Id: Iaf89e12f70bdb255c17180cbd0d30644afc1242f
    Signed-off-by: Kiran Kumar C.S.K <kkumarcs@codeaurora.org>

--- frontends/nss/ecm_nss_multicast_ipv4.c
+++ frontends/nss/ecm_nss_multicast_ipv4.c
@@ -3075,7 +3075,7 @@ static void ecm_mfc_update_event_callback(__be32 group, __be32 origin, uint32_t 
 	 */
 	ci = ecm_db_multicast_connection_find_and_ref(tuple_instance);
 	if (!ci) {
-		DEBUG_ASSERT(false, "%p: Bad connection instance for routed mcast flow\n", tuple_instance);
+		DEBUG_TRACE("%p: Bad connection instance for routed mcast flow\n", tuple_instance);
 		ecm_db_multicast_tuple_instance_deref(tuple_instance);
 		return;
 	}
