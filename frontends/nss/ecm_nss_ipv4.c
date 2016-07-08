576b84f3ead4bcea4a8d0677a3a5fb1101deec79
Amit Gupta <amitgupt@codeaurora.org>
2016-07-08 19:51:49 +0530

    [qca-nss-ecm] avoid acceleration for software tunnel device
    Avoid pushing rule for software tunnel and tap devices
    
    Change-Id: I0b6fde913864379ddc9d1d77f90f25741d122b8e
    Signed-off-by: Amit Gupta <amitgupt@codeaurora.org>

--- frontends/nss/ecm_nss_ipv4.c
+++ frontends/nss/ecm_nss_ipv4.c
@@ -1031,6 +1031,16 @@ static unsigned int ecm_nss_ipv4_ip_process(struct net_device *out_dev, struct n
 	}
 
 	/*
+	 * Do not accelerate flows to/from any virtual tunnel or tap devices.
+	 */
+	if ((in_dev->priv_flags & IFF_TUN_TAP) ||
+		(out_dev->priv_flags & IFF_TUN_TAP)) {
+
+		DEBUG_TRACE("virtual tunnels are not accelerated by ECM\n");
+		return NF_ACCEPT;
+	}
+
+	/*
 	 * Extract information, if we have conntrack then use that info as far as we can.
 	 */
         ct = nf_ct_get(skb, &ctinfo);
