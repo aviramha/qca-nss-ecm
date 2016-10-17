4687b9d27dbdd0aa4de056991f51d49bb63cb622
Murat Sezgin <msezgin@codeaurora.org>
2016-10-17 14:29:51 -0700

    [qca-nss-ecm] Limit stats request from NSS
    Do not request stats if there is no accelerated connection
    in the database. Delay 1 second to reschedule the request.
    
    Change-Id: Ic70081dcb5e199e699914799be14ef5a00066a8d
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- frontends/nss/ecm_nss_ipv4.c
+++ frontends/nss/ecm_nss_ipv4.c
@@ -2072,6 +2072,11 @@ static void ecm_nss_ipv4_stats_sync_req_work(struct work_struct *work)
 	int retry = 3;
 	unsigned long int current_jiffies;
 
+	if (ecm_nss_ipv4_accelerated_count == 0) {
+		DEBUG_TRACE("There is no accelerated IPv4 connection\n");
+		goto reschedule;
+	}
+
 	usleep_range(ECM_NSS_IPV4_STATS_SYNC_UDELAY - 100, ECM_NSS_IPV4_STATS_SYNC_UDELAY);
 
 	nicsm_req = &ecm_nss_ipv4_sync_req_msg->msg.conn_stats_many;
@@ -2101,7 +2106,7 @@ static void ecm_nss_ipv4_stats_sync_req_work(struct work_struct *work)
 		nss_tx_status = nss_ipv4_tx_with_size(ecm_nss_ipv4_nss_ipv4_mgr, ecm_nss_ipv4_sync_req_msg, PAGE_SIZE);
 		if (nss_tx_status == NSS_TX_SUCCESS) {
 			ecm_nss_ipv4_stats_request_success++;
-			return;
+			goto reschedule;
 		}
 		ecm_nss_ipv4_stats_request_fail++;
 		retry--;
@@ -2109,6 +2114,7 @@ static void ecm_nss_ipv4_stats_sync_req_work(struct work_struct *work)
 		usleep_range(100, 200);
 	}
 
+reschedule:
 	/*
 	 * TX failed after retries, reschedule ourselves with fresh start
 	 */
