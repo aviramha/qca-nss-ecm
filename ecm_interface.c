ac267641c1b3d16358682b6f7e04c347198bc871
Murat Sezgin <msezgin@codeaurora.org>
2016-11-02 15:55:11 -0700

    [qca-nss-ecm] Fix KW issue in wifi event handler thread
    Use the correct sizeof parameter.
    
    Change-Id: I077b9226641b79063cccc240edffa1a6eda1b06d
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- ecm_interface.c
+++ ecm_interface.c
@@ -5863,7 +5863,7 @@ static void ecm_interface_wifi_event_thread(void)
 		goto exit1;
 	}
 
-	memset(&saddr, 0, sizeof(struct sockaddr));
+	memset(&saddr, 0, sizeof(saddr));
 	saddr.nl_family = AF_NETLINK;
 	saddr.nl_groups = RTNLGRP_LINK;
 	saddr.nl_pid    = current->pid;
